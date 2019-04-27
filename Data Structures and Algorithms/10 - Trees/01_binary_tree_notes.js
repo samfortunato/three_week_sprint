af65c331c906510ae8498b54438607082c98e9fe
Samuel Fortunato <samfortunato@gmail.com>
2019-04-27 01:31:37 -0400

    Finish in-order, pre-order, and post-order print lessons

--- Data Structures and Algorithms/10 - Trees/01_binary_tree_notes.js
+++ Data Structures and Algorithms/10 - Trees/01_binary_tree_notes.js
@@ -6,6 +6,30 @@ class TreeNode {
   }
 }
 
+function inOrderPrint(root) {
+  if (root === null) return;
+
+  inOrderPrint(root.left);
+  console.log(root.value);
+  inOrderPrint(root.right);
+}
+
+function preOrderPrint(root) {
+  if (root === null) return;
+
+  console.log(root.value);
+  preOrderPrint(root.left);
+  preOrderPrint(root.right);
+}
+
+function postOrderPrint(root) {
+  if (root === null) return;
+
+  postOrderPrint(root.left);
+  postOrderPrint(root.right);
+  console.log(root.value);
+}
+
 const nodeA = new TreeNode('A');
 const nodeB = new TreeNode('B');
 const nodeC = new TreeNode('C');
