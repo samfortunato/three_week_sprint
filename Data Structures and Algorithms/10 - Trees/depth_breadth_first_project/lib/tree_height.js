533590c1349005e12f992311a2b8b6239566354a
Samuel Fortunato <samfortunato@gmail.com>
2019-04-28 16:44:41 -0400

    Implement recursive solutions with help from walkthrough

--- Data Structures and Algorithms/10 - Trees/depth_breadth_first_project/lib/tree_height.js
+++ Data Structures and Algorithms/10 - Trees/depth_breadth_first_project/lib/tree_height.js
@@ -1,23 +1,35 @@
+// iterative
+// function treeHeight(root) {
+//   if (root === null) return -1;
+
+//   const nodeStack = [root];
+//   let maxHeight = 0;
+
+//   while (nodeStack.length > 0) {
+//     const currentNode = nodeStack.pop();
+
+//     if (currentNode.left || currentNode.right) {
+//       maxHeight++;
+//     }
+
+//     if (currentNode.right) nodeStack.push(currentNode.right);
+//     if (currentNode.left) nodeStack.push(currentNode.left);
+//   }
+
+//   return maxHeight;
+// }
+
+// recursive
 function treeHeight(root) {
   if (root === null) return -1;
 
-  const nodeStack = [root];
-  let maxHeight = 0;
+  const height = Math.max(
+    treeHeight(root.left),
+    treeHeight(root.right)
+  );
 
-  while (nodeStack.length > 0) {
-    const currentNode = nodeStack.pop();
-
-    if (currentNode.left || currentNode.right) {
-      maxHeight++;
-    }
-
-    if (currentNode.right) nodeStack.push(currentNode.right);
-    if (currentNode.left) nodeStack.push(currentNode.left);
-  }
-
-  return maxHeight;
+  return height + 1;
 }
-
 
 module.exports = {
     treeHeight
--- Data Structures and Algorithms/10 - Trees/depth_breadth_first_project/lib/tree_sum.js
+++ Data Structures and Algorithms/10 - Trees/depth_breadth_first_project/lib/tree_sum.js
@@ -1,20 +1,27 @@
+// iterative
+// function treeSum(root) {
+//   if (root === null) return 0;
+
+//   const nodeStack = [root];
+//   let sum = 0;
+
+//   while (nodeStack.length > 0) {
+//     const currentNode = nodeStack.pop();
+//     sum += currentNode.val;
+
+//     if (currentNode.right) nodeStack.push(currentNode.right);
+//     if (currentNode.left) nodeStack.push(currentNode.left);
+//   }
+
+//   return sum;
+// }
+
+// recursive
 function treeSum(root) {
   if (root === null) return 0;
 
-  const nodeStack = [root];
-  let sum = 0;
-
-  while (nodeStack.length > 0) {
-    const currentNode = nodeStack.pop();
-    sum += currentNode.val;
-
-    if (currentNode.right) nodeStack.push(currentNode.right);
-    if (currentNode.left) nodeStack.push(currentNode.left);
-  }
-
-  return sum;
+  return root.val + treeSum(root.left) + treeSum(root.right);
 }
-
 
 module.exports = {
     treeSum
