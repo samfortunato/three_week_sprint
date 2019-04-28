b9faa7caf3361ce3f243dcee2d06df4dc8aa3289
Samuel Fortunato <samfortunato@gmail.com>
2019-04-28 00:22:14 -0400

    Reviewed solution to leetcode problem

--- Data Structures and Algorithms/10 - Trees/tree_order_project/lib/leet_code_105.js
+++ Data Structures and Algorithms/10 - Trees/tree_order_project/lib/leet_code_105.js
@@ -3,7 +3,30 @@
 
 const { TreeNode } = require('./tree_node.js');
 
-
 function buildTree(preorder, inorder) {
+  if (preorder.length === 0 && inorder.length === 0) {
+    return null;
+  }
+  
+  const rootVal = preorder[0];
+  const root = new TreeNode(rootVal);
 
+  const midIdx = inorder.indexOf(rootVal);
+  const leftInOrder = inorder.slice(0, midIdx);
+  const rightInOrder = inorder.slice(midIdx);
+
+  const leftPreOrder = preorder.filter((val) => {
+    leftInOrder.includes(val);
+  });
+  const rightPreOrder = preorder.filter((val) => {
+    rightInOrder.includes(val);
+  });
+
+  const leftTree = buildTree(leftPreOrder, leftInOrder);
+  const rightTree = buildTree(rightPreOrder, rightInOrder);
+
+  root.left = leftTree;
+  root.right = rightTree;
+
+  return root;
 }
--- Data Structures and Algorithms/10 - Trees/tree_order_project/lib/tree_order.js
+++ Data Structures and Algorithms/10 - Trees/tree_order_project/lib/tree_order.js
@@ -1,9 +1,27 @@
 function inOrderArray(root) {
+  if (root === null) return [];
 
+  let treeValues = [];
+
+  treeValues = treeValues
+    .concat(inOrderArray(root.left))
+    .concat(root.val)
+    .concat(inOrderArray(root.right));
+
+  return treeValues;
 }
 
 function postOrderArray(root) {
+  if (root === null) return [];
 
+  let treeValues = [];
+
+  treeValues = treeValues
+    .concat(postOrderArray(root.left))
+    .concat(postOrderArray(root.right))
+    .concat(root.val);
+
+  return treeValues;
 }
 
 
